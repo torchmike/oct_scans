98a0bcb65f498c6a20294bf021ea97009dc230f7
Michael Seiler <michaelseiler@Michaels-MacBook-Pro.local>
2017-09-18 19:03:31 -0400

    update to python script to generate stats

--- data/process.sh
+++ data/process.sh
@@ -2,4 +2,14 @@
 python header.py
 for fileprefix in $( ls *ratios*|cut -f1 -d\ |uniq); do
   cat *$fileprefix*gcl_rpe_ratios* | python process.py $fileprefix
-  done
+done
+
+python header.py
+for fileprefix in $( ls *ratios*|cut -f1 -d\ |uniq); do
+  cat *$fileprefix*gcl_values* | python process.py gcl_values$fileprefix
+done
+
+python header.py
+for fileprefix in $( ls *ratios*|cut -f1 -d\ |uniq); do
+  cat *$fileprefix*rpe_values* | python process.py rpe_values$fileprefix
+done
